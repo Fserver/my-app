6d7a1129eb8c01f04d18552aca1ca119d0eace79
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-06-03 12:21:20 -0500

    recientes cambios

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function Welcome(props) {
+
+    const { message, name } = props;
+    const { counter, setCounter } = useState(0);
+
+    return (
+        <div>
+            <h1>Hola {name}</h1>
+            <p>{message}</p>
+            <h3>Contador de React con Hooks {counter}</h3>
+            <button onClick={() => setCounter(counter + 1)}>Contar</button>
+        </div>
+    )
+}
