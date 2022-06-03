5dfef422b5a707719a291a68525940427244d59b
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-06-03 16:54:31 -0500

    corrección de errores.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,14 +3,14 @@ import { useState } from "react";
 export default function Welcome(props) {
 
     const { message, name } = props;
-    const { counter, setCounter } = useState(0);
+    const [ counter, setCounter ] = useState(0);
 
     return (
         <div>
             <h1>Hola {name}</h1>
             <p>{message}</p>
             <h3>Contador de React con Hooks {counter}</h3>
-            <button onClick={() => setCounter(counter + 1)}>Contar</button>
+            <button class="" onClick={() => setCounter(counter + 1)}>Contar</button>
         </div>
     )
 }
