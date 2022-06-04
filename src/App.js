ae79f39e4740d5f59043ef2d27725151183945bf
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-06-04 12:53:58 -0500

    Router DOM

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Contact from './pages/contact';
+import About from './pages/aboutMe';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <Welcome message="Hola desde props" name="Felipe"/>
+
+        <Router>
+
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contacto</Link>
+              </li>
+              <li>
+                <Link to="/about-me">Mi</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about-me" element={<About />} />
+          </Routes>
+
+        </Router>
+
 
       </header>
     </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Welcome(props) {
 
-    const { message, name } = props;
-    const [ counter, setCounter ] = useState(0);
+    const { message, name } = props
+    const [ semaforo , setSemaforo ] = useState(false)
+    const [ counter, setCounter ] = useState(0)
+    const [ counter2, setCounter2 ] = useState(0)
+
+    useEffect(()=>{
+        console.log("Semaforo cambio de estado a ",{semaforo});
+    }, [semaforo])
+
+    const contar = () => {
+        setCounter2(counter2+1)
+        setSemaforo(!semaforo)
+    }
 
     return (
         <div>
             <h1>Hola {name}</h1>
             <p>{message}</p>
             <h3>Contador de React con Hooks {counter}</h3>
-            <button class="" onClick={() => setCounter(counter + 1)}>Contar</button>
+            <h3>Contador Hooks Use Efects {counter2}</h3>
+            <h3>Semaforo en {semaforo ? 'Rojo' : 'Verde'}</h3>
+            <button onClick={() => setCounter(counter + 1)}>Contar</button>
+            <button onClick={contar}>Contar2</button>
         </div>
     )
 }
